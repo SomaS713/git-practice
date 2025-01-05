afc53538ef437ae3576c347f365069c7a8cc172f
SomaS713 <soma.srujan13@gmail.com>
2025-01-05 23:49:12 +0530

    conversation

---
+++ 2-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, what are you doing"
+echo "Suresh:: Hello Ramesh, Im doing nothing"
+echo "Ramesh:: which course is better for IT"
+echo "Suresh:: Looks like Devops is booming in India"
