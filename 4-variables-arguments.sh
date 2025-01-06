058ef8755354e5ec9a1f449a50703180d5a0529d
SomaS713 <soma.srujan13@gmail.com>
2025-01-06 16:14:31 +0530

    variables-arguements

---
+++ 4-variables-arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, what are you doing"
+echo "$PERSON2:: Hello $PERSON1, Im doing nothing"
+echo "$PERSON1:: which course is better for IT"
+echo "$PERSON2:: Looks like Devops is booming in India"
