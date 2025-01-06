0f96b5688791da5187371450e4fb65ce19a6a903
SomaS713 <soma.srujan13@gmail.com>
2025-01-06 16:22:04 +0530

    variables-arguements

--- 5-login-pass.sh
+++ 5-login-pass.sh
@@ -2,6 +2,10 @@
 
 echo "please enter your username::"
 
-read USERNAME
+read USERNAME #takes input
 
 echo "username entered as:: $USERNAME"
+
+echo "enter your password"
+read -s PASSWORD 
+echo "Password entered as $PASSWORD"
