f19d726f5505f72a78ad354de219758d86bfe8d5
SomaS713 <soma.srujan13@gmail.com>
2025-01-06 16:29:15 +0530

    variables-arguements

---
+++ 6-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#index/position starts from 0 
+#size is 3(elements)
+
+FRUITS=("APPLE" "BANANA" "ORANGE") #array=list of names
+
+echo "first fruit is $FRUITS[0]"
+echo "first fruit is $FRUITS[1]"
+echo "first fruit is $FRUITS[2]"
+
+echo "here is my all fruits:: $FRUITS[@]"
