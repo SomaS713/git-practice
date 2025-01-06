c6685a20fc7ae7d63c59ac9ae9cdde6e2b419342
SomaS713 <soma.srujan13@gmail.com>
2025-01-06 16:06:54 +0530

    variables

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2, what are you doing"
+echo "$PERSON2:: Hello $PERSON1, Im doing nothing"
+echo "$PERSON1:: which course is better for IT"
+echo "$PERSON2:: Looks like Devops is booming in India"
